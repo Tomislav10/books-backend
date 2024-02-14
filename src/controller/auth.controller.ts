243af95f78dc827cbca04b516b056f8c1a81c37f
Tomislav <>
2024-02-14 04:38:24 +0100

    Basic login functionality set

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -23,3 +23,28 @@ export const Register = async (req: Request, res: Response) => {
 
     res.send(user);
 }
+
+export const Login = async (req: Request, res: Response) => {
+    console.log('aaaa');
+    const {first_name, last_name, email, password, password_confirm} = req.body;
+
+    const user = await getRepository(UserEntity).findOne({
+        where: {
+            email: email,
+        },
+    });
+
+    if(!user) {
+        return res.status(404).send({
+            message: 'Not found. Invalid credentials!!!!! '
+        })
+    }
+
+    if(!await bcryptjs.compare(req.body.password, user.password)) {
+        return res.status(400).send({
+            message: 'Wrong pass. Invalid credentials!!!!! '
+        })
+    }
+
+    res.send(user);
+}
