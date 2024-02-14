d185381c26bdbd36ee25637d2f4d7b099e6ba84b
Tomislav <>
2024-02-14 03:22:41 +0100

    express added and listen port adjusted

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
+import express, {Request, Response} from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('tessstttt!!!!')
+});
+
+app.listen(8000, () => {
+    console.log('listttteeenn 8000')
+})
+
 console.log('hello55')
